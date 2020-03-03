b59cb91536b069bdaad154cf1c67d5574b900477
Laszlo Pusok <laszlo.pusok@bitrise.io>
2020-03-03 16:01:22 +0100

    Initial commit

---
+++ parallel-ui-test-genTests/parallel_ui_test_genTests.swift
@@ -0,0 +1,34 @@
+//
+//  parallel_ui_test_genTests.swift
+//  parallel-ui-test-genTests
+//
+//  Created by lpusok on 2020. 02. 28..
+//  Copyright © 2020. bitrise. All rights reserved.
+//
+
+import XCTest
+@testable import parallel_ui_test_gen
+
+class parallel_ui_test_genTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
